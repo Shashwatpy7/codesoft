7719d076353d91f8ff5227d40767f99890d51b68
Shashwat <pshashwat731@gmail.com>
2025-07-01 19:03:21 +0530

    Update app.py with deployment changes

--- password-generator/app.py
+++ password-generator/app.py
@@ -29,4 +29,4 @@ def home():
     return render_template('home.html',form = form,passwords = passwords)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0',port=10000)
